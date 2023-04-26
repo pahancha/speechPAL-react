fe064985eddba2170c4fb43fcf9ff36fea138599
pahancha <pahanchathuranga123@gmail.com>
2023-04-26 09:24:44 +0530

    client is properly communicating with the flask back-end.

--- src/components/SpeechRecognitionButton.jsx
+++ src/components/SpeechRecognitionButton.jsx
@@ -1,21 +1,90 @@
 import React, { useState } from 'react';
 import './SpeechRecognitionButton.css';
+import TranscribedText from './TranscribedText';
 
 function SpeechRecognitionButton() {
   const [isRecording, setIsRecording] = useState(false);
+  const [transcribedText, setTranscribedText] = useState("");
+  const [transcribing, setTranscribing] = useState(false);
+  const [transcribedTextLoaded, setTranscribedTextLoaded] = useState(false);
+  // recording related
+  const [audioStream, setAudioStream] = useState(null);
+  const [recordedData, setRecordedData] = useState(null);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
 
-  function handleButtonClick() {
-    setIsRecording(!isRecording);
-  }
+  const handleStartRecording = () => {
+    setIsRecording(true);
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const recorder = new MediaRecorder(stream);
+      recorder.start();
+
+      setMediaRecorder(recorder);
+      setAudioStream(stream);
+
+      const chunks = [];
+      recorder.addEventListener("dataavailable", (event) => {
+        chunks.push(event.data);
+      });
+
+      recorder.addEventListener("stop", () => {
+        const blob = new Blob(chunks, { type: "audio/wav" });
+        setRecordedData(blob);
+        setAudioStream(null);
+      });
+    });
+  };
+
+  const handleStopRecording = async () => {
+    setIsRecording(false);
+    console.log("Recording stopped.");
+
+    if (audioStream) {
+      audioStream.getTracks().forEach((track) => {
+        track.stop();
+      });
+
+      if (mediaRecorder) {
+        mediaRecorder.ondataavailable = (e) => {
+          const blob = e.data;
+          const formData = new FormData();
+          formData.append("audio", blob, "recording.wav");
+
+          fetch("http://127.0.0.1:5000/generateASR", {
+            method: "POST",
+            body: formData,
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              // Handle transcribed text from Flask back-end
+              console.log(data);
+              const generatedSentence = data.results[0].generatedSentece;
+              const originalSpeech = data.results[0].originalSpeech;
+
+              // Handle transcribed text from Flask back-end
+              console.log("Generated sentence: ", generatedSentence);
+              console.log("Original speech: ", originalSpeech);
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        };
+
+        mediaRecorder.stop();
+      }
+    }
+  };
 
   return (
     <div className="button-container">
       <button
         className={`speech-button ${isRecording ? 'recording' : ''}`}
-        onClick={handleButtonClick}
+        onClick={isRecording ? handleStopRecording : handleStartRecording}
       >
         {isRecording ? 'Stop' : 'Click here to speak'}
       </button>
+      {transcribing && <div>Transcribing...</div>}
+      {transcribedTextLoaded && <TranscribedText text={transcribedText} />}  
+      <div></div>
     </div>
   );
 }
