fef54470a5539dc82f6f2b4c80cd432b6331ecd4
pahancha <pahanchathuranga123@gmail.com>
2023-05-26 16:38:31 +0530

    code optimiz: SelectClassButton

--- src/components/SelectClassButtons.jsx
+++ src/components/SelectClassButtons.jsx
@@ -4,7 +4,6 @@ import './SelectClassButtons.css';
 function SelectClassButtons() {
   const [selectedButton, setSelectedButton] = useState(null);
   const [context, setContext] = useState('');
-  
 
   useEffect(() => {
     console.log(context);
@@ -15,63 +14,24 @@ function SelectClassButtons() {
     setContext(buttonName);
   };
 
+  const buttons = ['need', 'inform', 'preference', 'question', 'general', 'all'];
+
   return (
     <div className="sbc-select-class-buttons">
-      <button
-        className={`sbc-button ${
-          selectedButton === 'need' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('need')}
-      >
-        Need
-        {selectedButton === 'need' && <span className="sbc-selected-text">Selected</span>}
-      </button>
-      <button
-        className={`sbc-button ${
-          selectedButton === 'inform' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('inform')}
-      >
-        Inform
-        {selectedButton === 'inform' && <span className="sbc-selected-text">Selected</span>}
-      </button>
-      <button
-        className={`sbc-button ${
-          selectedButton === 'preference' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('preference')}
-      >
-        Preference
-        {selectedButton === 'preference' && <span className="sbc-selected-text">Selected</span>}
-      </button>
-      <button
-        className={`sbc-button ${
-          selectedButton === 'question' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('question')}
-      >
-        Question
-        {selectedButton === 'question' && <span className="sbc-selected-text">Selected</span>}
-      </button>
-      <button
-        className={`sbc-button ${
-          selectedButton === 'general' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('general')}
-      >
-        General
-        {selectedButton === 'general' && <span className="sbc-selected-text">Selected</span>}
-      </button>
-
-      <button
-        className={`sbc-button sbc-all ${
-          selectedButton === 'all' ? 'sbc-selected' : ''
-        }`}
-        onClick={() => handleButtonClick('all')}
-      >
-        All
-        {selectedButton === 'all' && <span className="sbc-selected-text">Selected</span>}
-      </button>
+      {buttons.map((buttonName) => (
+        <button
+          key={buttonName}
+          className={`sbc-button ${
+            selectedButton === buttonName ? 'sbc-selected' : ''
+          } ${buttonName === 'all' ? 'sbc-all' : ''}`}
+          onClick={() => handleButtonClick(buttonName)}
+        >
+          {buttonName.charAt(0).toUpperCase() + buttonName.slice(1)}
+          {selectedButton === buttonName && (
+            <span className="sbc-selected-text">Selected</span>
+          )}
+        </button>
+      ))}
     </div>
   );
 }
